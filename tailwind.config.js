9edee1cb7d6fbd95e1f50b31e66f42bcc9b8fea6
bpartho <bpartho.neo@gmail.com>
2024-09-23 01:46:58 +0600

    apexa_website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+ 
+    theme: {
+      extend: {
+        animation: {
+          'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        },
+        keyframes: {
+          'infinite-scroll': {
+            from: { transform: 'translateX(0)' },
+            to: { transform: 'translateX(-100%)' },
+          }
+        }                    
+      },
+    },
+  
+  plugins: [],
+}
+
