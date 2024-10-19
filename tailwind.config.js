795c71c0f9b1c5b25890c2184f3a0be9adb56015
bpartho <bpaartho.neo@gmail.com>
2024-10-19 12:12:25 +0100

    add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,27 @@ module.exports = {
       extend: {
         animation: {
           'infinite-scroll': 'infinite-scroll 25s linear infinite',
+          'move': ' 3s linear infinite',
         },
         keyframes: {
+          'move': {
+          '0%': {
+            transform: translateX(0)
+          },
+          '25%': {
+            transform: translateX('400px')
+          },
+          '50%': {
+            transform: translateX(0)
+          },
+          '75%': {
+            transform: translateX('-400px')
+          },
+          '100%': {
+            transform: translateX(0)
+          }
+
+          },
           'infinite-scroll': {
             from: { transform: 'translateX(0)' },
             to: { transform: 'translateX(-100%)' },
